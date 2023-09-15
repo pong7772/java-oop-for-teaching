3b44050aa9bf3a0a8abe7e1a6a4fe43990b34dbc
FoedRupp <foedrupp@FoedRupps-iMac.local>
2023-09-15 17:50:01 +0700

    oop1

--- OOPEx1.java
+++ OOPEx1.java
@@ -5,19 +5,20 @@ public class OOPEx1 {
         s1.score = 90.0;
         s1.name = "sokai";
         s1.show();
+        System.out.println(s1.getResult());
 
         Student s2 = new Student();
         s2.age = 200;
         s2.name = "sokai2";
         s2.score = 80.0;
         s2.show();
-
+        System.out.println(s2.getResult());
         Student s3 = new Student();
         s3.name = "sokai3";
         s3.age = 300;
         s3.score = 70.0;
         s3.show();
-
+        System.out.println(s3.getResult());
     }
 }
 
@@ -25,8 +26,21 @@ public class OOPEx1 {
     String name ;
     int age ;
     double score ;
-    
+    public String  getResult(){
+        if (score > 100){
+            return "A";
+        }else if (score > 80){
+            return "B";
+        }else if (score > 60){
+            return "C";
+        }else if (score > 40){
+            return "D";
+        }else {
+            return "F";
+        }
+    }
     public void show() {
         System.out.println("Name: " + name + " Age: " + age + " Score: " + score);
     }
+    
 }
