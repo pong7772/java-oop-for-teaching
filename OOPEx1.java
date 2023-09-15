b556067414563a3faf3ac5d9ad75b5da6831b138
FoedRupp <foedrupp@FoedRupps-iMac.local>
2023-09-15 17:29:16 +0700

    update

--- OOPEx1.java
+++ OOPEx1.java
@@ -3,14 +3,17 @@ public class OOPEx1 {
         Student s1 = new Student();
         s1.age = 100;
         s1.score = 90.0;
+        s1.name = "sokai";
         s1.show();
 
         Student s2 = new Student();
         s2.age = 200;
+        s2.name = "sokai2";
         s2.score = 80.0;
         s2.show();
 
         Student s3 = new Student();
+        s3.name = "sokai3";
         s3.age = 300;
         s3.score = 70.0;
         s3.show();
